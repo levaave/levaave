958470eefc5896180e0c3c1cf2e36ea7f9b32689
marcoruggeri <4high20@gmail.com>
2021-01-23 22:16:26 +0000

    Contract switch to 1inch v2

--- packages/hardhat/contracts/LevAave.sol
+++ packages/hardhat/contracts/LevAave.sol
@@ -1,31 +1,12 @@
 pragma solidity 0.6.12;
 
-import { FlashLoanReceiverBase } from "./utils/FlashLoanReceiverBase.sol";
-import { ILendingPool } from "./interfaces/ILendingPool.sol";
-import { ILendingPoolAddressesProvider } from "./interfaces/ILendingPoolAddressesProvider.sol";
-import { IERC20 } from "./interfaces/IERC20.sol";
-
-interface IOneSplit {
-    function getExpectedReturn(
-        IERC20,
-        IERC20,
-        uint256,
-        uint256,
-        uint256
-    ) external view returns (uint256, uint256[] memory);
-
-    function swap(
-        IERC20,
-        IERC20,
-        uint256,
-        uint256,
-        uint256[] memory,
-        uint256
-    ) external payable returns (uint256);
-}
+import { FlashLoanReceiverBase } from "./FlashLoanReceiverBase.sol";
+import { ILendingPool } from "./ILendingPool.sol";
+import { ILendingPoolAddressesProvider } from "./ILendingPoolAddressesProvider.sol";
+import { IERC20 } from "./IERC20.sol";
 
 contract LevAave is FlashLoanReceiverBase {
-    IOneSplit oneInch = IOneSplit(0x50FDA034C0Ce7a8f7EFDAebDA7Aa7cA21CC1267e);
+    address oneInch = address(0x111111125434b319222CdBf8C261674aDB56F3ae);
     ILendingPool pool = ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
 
     constructor(ILendingPoolAddressesProvider _addressProvider) public FlashLoanReceiverBase(_addressProvider) {}
@@ -41,8 +22,10 @@ contract LevAave is FlashLoanReceiverBase {
         uint256 amount;
         address asset;
         uint256 operation;
-        address collateralAsset;
         uint256 collateralAmount;
+        bool success;
+        bytes oneInchData;
+        bytes oneInchApprove;
     }
 
     function executeOperation(
@@ -60,30 +43,22 @@ contract LevAave is FlashLoanReceiverBase {
             slot.sender,
             slot.positionAsset,
             slot.apositionAsset,
-            slot.collateralAsset,
             slot.operation,
-            slot.collateralAmount
-        ) = abi.decode(params, (address, address, address, address, uint256, uint256));
+            slot.collateralAmount,
+            slot.oneInchData,
+            slot.oneInchApprove
+        ) = abi.decode(params, (address, address, address, uint256, uint256, bytes, bytes));
         //open long position
         if (slot.operation == 0) {
-            // e.g. loanasset = weth = collateral asset , position asset = link
-
             // transfer collateral from user to contract
-            // send over eth that is equal to half the loaned. loan 2k usdc, transfer 1k usdc.
             IERC20(slot.asset).transferFrom(slot.sender, address(this), slot.amount.div(2));
             // update collateral balance
-            slot.balance = IERC20(slot.asset).balanceOf(address(this)); //3k usdc
-            // get 1inch v1 distribution
-            (uint256 returnAmount, uint256[] memory distribution) =
-                oneInch.getExpectedReturn(IERC20(assets[0]), IERC20(slot.positionAsset), slot.balance, 10, 0);
-            // if no 1inch allowance, approve
-            if (IERC20(slot.asset).allowance(address(this), address(oneInch)) < slot.balance) {
-                IERC20(slot.asset).approve(address(oneInch), uint256(-1));
-            }
-            // 1inch swap
-            oneInch.swap(IERC20(slot.asset), IERC20(slot.positionAsset), slot.balance, 1, distribution, 0);
-            // swap 3k usdc -> eth
-            
+            slot.balance = IERC20(slot.asset).balanceOf(address(this));
+            // approve for 1inch
+            address(slot.asset).call(slot.oneInchApprove);
+            // 1inch trade
+            (slot.success, ) = oneInch.call(slot.oneInchData);
+            require(slot.success, "1inch call failed");
             // if no aave allowance, approve
             if (IERC20(slot.positionAsset).allowance(address(this), address(pool)) < slot.balance) {
                 IERC20(slot.positionAsset).approve(address(pool), uint256(-1));
@@ -100,21 +75,18 @@ contract LevAave is FlashLoanReceiverBase {
             if (IERC20(slot.asset).allowance(address(this), address(pool)) < slot.amount) {
                 IERC20(slot.asset).approve(address(pool), uint256(-1));
             }
-            // first repaid the debt of the user using the flashloan funds
+            // first repay the debt of the user using the flashloan funds
             pool.repay(slot.asset, slot.amount, 2, slot.sender);
             // transfer atoken from user to contract
-            IERC20(slot.apositionAsset).transferFrom(slot.sender, address(this), slot.collateralAmount);
+            uint256 collateralBalance = IERC20(slot.apositionAsset).balanceOf(slot.sender);
+            IERC20(slot.apositionAsset).transferFrom(slot.sender, address(this), collateralBalance);
             // transform atoken in token
-            pool.withdraw(slot.positionAsset, slot.collateralAmount, address(this));
-            // get 1inch v1 distribution
-            (uint256 returnAmount, uint256[] memory distribution) =
-                oneInch.getExpectedReturn(IERC20(slot.positionAsset), IERC20(slot.asset), slot.collateralAmount, 10, 0);
-            // if no 1inch allowance, approve
-            if (IERC20(slot.positionAsset).allowance(address(this), address(oneInch)) < slot.collateralAmount) {
-                IERC20(slot.positionAsset).approve(address(oneInch), uint256(-1));
-            }
-            // 1inch swap
-            oneInch.swap(IERC20(slot.positionAsset), IERC20(slot.asset), slot.collateralAmount, 1, distribution, 0);
+            pool.withdraw(slot.positionAsset, collateralBalance, address(this));
+            // approve for 1inch
+            address(slot.positionAsset).call(slot.oneInchApprove);
+            // 1inch trade
+            (slot.success, ) = oneInch.call(slot.oneInchData);
+            require(slot.success, "1inch call failed");
             // send collateral back to user
             slot.balance = IERC20(slot.asset).balanceOf(address(this));
             IERC20(slot.asset).transfer(slot.sender, slot.balance - slot.amount.add(premiums[0]));
@@ -123,24 +95,20 @@ contract LevAave is FlashLoanReceiverBase {
         // open short position
         if (slot.operation == 2) {
             // transfer collateral from user to contract
-            IERC20(slot.collateralAsset).transferFrom(slot.sender, address(this), slot.collateralAmount);
-            // get 1inch v1 distribution
-            (uint256 returnAmount, uint256[] memory distribution) =
-                oneInch.getExpectedReturn(IERC20(slot.asset), IERC20(slot.collateralAsset), slot.amount, 10, 0);
-            // if no 1inch allowance, approve
-            if (IERC20(slot.asset).allowance(address(this), address(oneInch)) < slot.amount) {
-                IERC20(slot.asset).approve(address(oneInch), uint256(-1));
-            }
-            // 1inch swap
-            oneInch.swap(IERC20(slot.asset), IERC20(slot.collateralAsset), slot.amount, 1, distribution, 0);
+            IERC20(slot.positionAsset).transferFrom(slot.sender, address(this), slot.collateralAmount);
+            // 1inch approve
+            address(slot.asset).call(slot.oneInchApprove);
+            // 1inch trade
+            (slot.success, ) = oneInch.call(slot.oneInchData);
+            require(slot.success, "1inch call failed");
             // update collateral balance
-            slot.balance = IERC20(slot.collateralAsset).balanceOf(address(this));
+            slot.balance = IERC20(slot.positionAsset).balanceOf(address(this));
             // if no aave allowance, approve
-            if (IERC20(slot.collateralAsset).allowance(address(this), address(pool)) < slot.balance) {
-                IERC20(slot.collateralAsset).approve(address(pool), uint256(-1));
+            if (IERC20(slot.positionAsset).allowance(address(this), address(pool)) < slot.balance) {
+                IERC20(slot.positionAsset).approve(address(pool), uint256(-1));
             }
             // deposit collateral to aave on behalf of user
-            pool.deposit(slot.collateralAsset, slot.balance, slot.sender, 0);
+            pool.deposit(slot.positionAsset, slot.balance, slot.sender, 0);
             // borrow loaned asset on behalf of user
             pool.borrow(slot.asset, slot.amount.add(premiums[0]), 2, 0, slot.sender);
             // transfer loan to pay back from user to contract
@@ -157,21 +125,27 @@ contract LevAave is FlashLoanReceiverBase {
     }
 
     function myFlashLoanCall(
-        address loanAsset, //usdc
-        address positionAsset, //eth
-        address apositionAsset, //eth
+        address loanAsset,
+        address positionAsset,
+        address apositionAsset,
+        address debtAsset,
         uint256 amount,
         uint256 operation,
-        address collateralAsset,
-        uint256 collateralAmount
+        uint256 collateralAmount,
+        bytes calldata oneInchData,
+        bytes calldata oneInchApprove
     ) public {
-        address receiverAddress = address(this);
-
         address[] memory assets = new address[](1);
         assets[0] = loanAsset;
 
         uint256[] memory amounts = new uint256[](1);
-        amounts[0] = amount + 10000;
+        if (operation == 0 || operation == 2) {
+            amounts[0] = amount;
+        }
+        if (operation == 1) {
+            uint256 debt = IERC20(debtAsset).balanceOf(msg.sender);
+            amounts[0] = debt;
+        }
 
         // 0 = no debt, 1 = stable, 2 = variable
         uint256[] memory modes = new uint256[](1);
@@ -179,9 +153,17 @@ contract LevAave is FlashLoanReceiverBase {
 
         address onBehalfOf = address(this);
         bytes memory params =
-            abi.encode(msg.sender, positionAsset, apositionAsset, collateralAsset, operation, collateralAmount);
+            abi.encode(
+                msg.sender,
+                positionAsset,
+                apositionAsset,
+                operation,
+                collateralAmount,
+                oneInchData,
+                oneInchApprove
+            );
         uint16 referralCode = 0;
 
-        LENDING_POOL.flashLoan(receiverAddress, assets, amounts, modes, onBehalfOf, params, referralCode);
+        LENDING_POOL.flashLoan(address(this), assets, amounts, modes, onBehalfOf, params, referralCode);
     }
 }
--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -1,12 +1,16 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const fetch = require("node-fetch");
 
 let lendingPool;
 let weth;
 let aweth;
 let usdc;
+let dai;
 let uniswap;
 let usdcDebt;
+let daiDebt;
+let calldata;
 
 describe("levaave", function () {
   before(async () => {
@@ -18,9 +22,15 @@ describe("levaave", function () {
     aweth = AWeth.attach("0x030bA81f1c18d280636F32af80b9AAd02Cf0854e");
     const Usdc = await ethers.getContractFactory("WETH9");
     usdc = Usdc.attach("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48");
+    const Dai = await ethers.getContractFactory("WETH9");
+    dai = Dai.attach("0x6B175474E89094C44Da98b954EedeAC495271d0F");
     usdcDebt = await ethers.getContractAt(
       "DebtTokenBase",
       "0x619beb58998eD2278e08620f97007e1116D5D25b"
+    );
+    daiDebt = await ethers.getContractAt(
+      "DebtTokenBase",
+      "0x6C3c78838c761c6Ac7bE9F59fe808ea2A6E4379d"
     );
     wethDebt = await ethers.getContractAt(
       "DebtTokenBase",
@@ -40,15 +50,15 @@ describe("levaave", function () {
   });
   it("open long position", async function () {
     this.timeout(120000);
-    balance = await usdc.balanceOf(deployer.getAddress());
-    console.log("jsbalance", ethers.utils.formatUnits(balance, 6));
+    balance = await dai.balanceOf(deployer.getAddress());
+    console.log("jsbalance", ethers.utils.formatUnits(balance));
     txApprove = await weth.approve(
       "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
       ethers.utils.parseEther("10000000")
     );
     await txApprove.wait();
     txDeposit = await weth.deposit({ value: ethers.utils.parseEther("100") });
-    usdc.approve(flashloan.address, ethers.utils.parseEther("1000000000"));
+    dai.approve(flashloan.address, ethers.utils.parseEther("1000000000"));
     txApproveUni = await weth.approve(
       "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
       ethers.utils.parseEther("10000000")
@@ -58,42 +68,52 @@ describe("levaave", function () {
       1,
       [
         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+        "0x6B175474E89094C44Da98b954EedeAC495271d0F",
       ],
       deployer.getAddress(),
       999999999999999,
       { value: ethers.utils.parseEther("10") }
     );
     await txConvert.wait();
-    balance2 = await usdc.balanceOf(deployer.getAddress());
-    console.log("jsbalance2", ethers.utils.formatUnits(balance2, 6));
-    txApproveLoan = await usdc.approve(
+    balance2 = await dai.balanceOf(deployer.getAddress());
+    console.log("jsbalance2", ethers.utils.formatUnits(balance2));
+    txApproveLoan = await dai.approve(
       flashloan.address,
-      ethers.utils.parseUnits("1000000000000000")
+      ethers.utils.parseUnits("10000000000")
     );
     await txApproveLoan.wait();
     txApproveWeth = await aweth.approve(
       flashloan.address,
-      ethers.utils.parseUnits("1000000000000000")
+      ethers.utils.parseUnits("10000000000")
     );
     await txApproveWeth.wait();
-    txDelegate = await usdcDebt.approveDelegation(
+    txDelegate = await daiDebt.approveDelegation(
       flashloan.address,
-      ethers.utils.parseUnits("1000000000000000")
+      ethers.utils.parseUnits("1000000000")
     );
     await txDelegate.wait();
+    let approveApi = await fetch(
+      "https://api.1inch.exchange/v2.0/approve/calldata?infinity=true&tokenAddress=0x6B175474E89094C44Da98b954EedeAC495271d0F"
+    );
+    let dataApprove = await approveApi.json();
+    let tradeApi = await fetch(
+      "https://api.1inch.exchange/v2.0/swap?fromTokenAddress=0x6B175474E89094C44Da98b954EedeAC495271d0F&toTokenAddress=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&amount=3000000000000000000000&fromAddress=0x5FbDB2315678afecb367f032d93F642f64180aa3&slippage=1&disableEstimate=true"
+    );
+    let dataTrade = await tradeApi.json();
     txLoan = await flashloan.myFlashLoanCall(
-      "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      "0x6B175474E89094C44Da98b954EedeAC495271d0F",
       "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
       "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e",
-      ethers.utils.parseUnits("2000", 6),
+      "0x6C3c78838c761c6Ac7bE9F59fe808ea2A6E4379d",
+      ethers.utils.parseUnits("2000"),
       0,
-      "0x0000000000000000000000000000000000000000",
-      0
+      0,
+      dataTrade.tx.data,
+      dataApprove.data
     );
     await txLoan.wait();
     balance3 = await usdc.balanceOf(deployer.getAddress());
-    console.log("jsbalance3", ethers.utils.formatUnits(balance3, 6));
+    console.log("jsbalance3", ethers.utils.formatUnits(balance3));
     info = await lendingPool.getUserAccountData(deployer.getAddress());
     console.log(
       "totEthColl",
@@ -113,20 +133,36 @@ describe("levaave", function () {
   });
   it("close long position", async function () {
     this.timeout(120000);
-    // txApproveLoan = await aweth.approve(flashloan.address, ethers.utils.parseUnits("1000000000000000"));
-    // await txApproveLoan.wait();
+    txApproveLoan = await aweth.approve(
+      flashloan.address,
+      ethers.utils.parseUnits("1000000000000000")
+    );
+    await txApproveLoan.wait();
     closeLongAmount = await aweth.balanceOf(deployer.getAddress());
-    repayAmount = await usdcDebt.balanceOf(deployer.getAddress());
-    console.log("repayAmount", ethers.utils.formatUnits(repayAmount, 6));
+    console.log("close1", closeLongAmount);
+    closeLongAmount = closeLongAmount.toString();
+    console.log("close2", closeLongAmount);
+    repayAmount = await daiDebt.balanceOf(deployer.getAddress());
+    console.log("repayAmount", ethers.utils.formatUnits(repayAmount));
     console.log("closeLongAmount", ethers.utils.formatUnits(closeLongAmount));
+    let approveApi = await fetch(
+      "https://api.1inch.exchange/v2.0/approve/calldata?infinity=true&tokenAddress=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    );
+    let dataApprove = await approveApi.json();
+    let tradeApi = await fetch(
+      `https://api.1inch.exchange/v2.0/swap?fromTokenAddress=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&toTokenAddress=0x6B175474E89094C44Da98b954EedeAC495271d0F&amount=${closeLongAmount}&fromAddress=0x5FbDB2315678afecb367f032d93F642f64180aa3&slippage=1&disableEstimate=true`
+    );
+    let dataTrade = await tradeApi.json();
     txClose = await flashloan.myFlashLoanCall(
-      "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+      "0x6B175474E89094C44Da98b954EedeAC495271d0F",
       "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
       "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e",
+      "0x6C3c78838c761c6Ac7bE9F59fe808ea2A6E4379d",
       repayAmount,
       1,
-      "0x0000000000000000000000000000000000000000",
-      closeLongAmount
+      0,
+      dataTrade.tx.data,
+      dataApprove.data
     );
     balance3 = await usdc.balanceOf(deployer.getAddress());
     console.log("jsbalance3", ethers.utils.formatUnits(balance3, 6));
@@ -148,8 +184,8 @@ describe("levaave", function () {
     console.log("healthFactor:", ethers.utils.formatEther(info.healthFactor));
   });
   it("open short position", async function () {
-    balance3 = await usdc.balanceOf(deployer.getAddress());
-    console.log("usdcbalance", ethers.utils.formatUnits(balance3, 6));
+    balance3 = await dai.balanceOf(deployer.getAddress());
+    console.log("usdcbalance", ethers.utils.formatUnits(balance3));
     txDelegate = await wethDebt.approveDelegation(
       flashloan.address,
       ethers.utils.parseUnits("1000000000000000")
@@ -160,18 +196,28 @@ describe("levaave", function () {
       ethers.utils.parseEther("10000000")
     );
     await txApprove.wait();
+    let approveApi = await fetch(
+      "https://api.1inch.exchange/v2.0/approve/calldata?infinity=true&tokenAddress=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    );
+    let dataApprove = await approveApi.json();
+    let tradeApi = await fetch(
+      `https://api.1inch.exchange/v2.0/swap?fromTokenAddress=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&toTokenAddress=0x6B175474E89094C44Da98b954EedeAC495271d0F&amount=2000000000000000000&fromAddress=0x5FbDB2315678afecb367f032d93F642f64180aa3&slippage=1&disableEstimate=true`
+    );
+    let dataTrade = await tradeApi.json();
     txShort = await flashloan.myFlashLoanCall(
       "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+      "0x6B175474E89094C44Da98b954EedeAC495271d0F",
       "0x0000000000000000000000000000000000000000",
       "0x0000000000000000000000000000000000000000",
-      ethers.utils.parseEther("2"),
+      ethers.utils.parseUnits("2"),
       2,
-      "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-      ethers.utils.parseUnits("2000", 6)
+      ethers.utils.parseUnits("1000"),
+      dataTrade.tx.data,
+      dataApprove.data
     );
     await txShort.wait();
-    balance3 = await usdc.balanceOf(deployer.getAddress());
-    console.log("usdcbalance", ethers.utils.formatUnits(balance3, 6));
+    balance3 = await dai.balanceOf(deployer.getAddress());
+    console.log("usdcbalance", ethers.utils.formatUnits(balance3));
     info = await lendingPool.getUserAccountData(deployer.getAddress());
     console.log(
       "totEthColl",
@@ -189,5 +235,5 @@ describe("levaave", function () {
     console.log("ltv:", ethers.utils.formatEther(info.ltv));
     console.log("healthFactor:", ethers.utils.formatEther(info.healthFactor));
   });
-  it("close short position", async function () {});
+  xit("close short position", async function () {});
 });
