467deadcbe7db5ff4ba958f2be5bd054f4bea516
Vamsi <vamsireddy9672@gmail.com>
2021-01-28 02:18:00 +0530

    new styles on UI

--- packages/react-app/src/helpers/abiHelpers.js
+++ packages/react-app/src/helpers/abiHelpers.js
@@ -10,6 +10,140 @@ export async function getListOfTokensSupportedByAAVE(contract) {
   }
 }
 
+export const tokenData = {
+  USDT: {
+    address: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    logo: "https://tokens.1inch.exchange/0xdac17f958d2ee523a2206206994597c13d831ec7.png",
+    decimal: 6,
+    name: "Tether Usd",
+  },
+  WBTC: {
+    address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    logo: "https://tokens.1inch.exchange/0x2260fac5e5542a773aa44fbcfedf7c193bc2c599.png",
+    decimal: 8,
+    name: "Wrapped BTC",
+  },
+  WETH: {
+    address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    logo: "https://tokens.1inch.exchange/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2.png",
+    name: "Wrapped Ether",
+    decimal: 8
+  },
+  YFI: {
+    address: "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
+    decimal: 18,
+    name: "yearn.finance",
+    logo: "https://tokens.1inch.exchange/0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e.png"
+  },
+  ZRX: {
+    address: "0xE41d2489571d322189246DaFA5ebDe1F4699F498",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0xe41d2489571d322189246dafa5ebde1f4699f498.png",
+    name: "0x Protocol"
+  },
+  UNI: {
+    address: "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0x1f9840a85d5af5bf1d1762f925bdaddc4201f984.png",
+    name: "UniSwap",
+  },
+  AAVE: {
+    address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+    name: "AAVE Token",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9.png",
+  },
+  BAT: {
+    address: "0x0D8775F648430679A709E98d2b0Cb6250d2887EF",
+    decimal: 18,
+    name: "Basic Attention Token",
+    logo: "https://tokens.1inch.exchange/0x0d8775f648430679a709e98d2b0cb6250d2887ef.png",
+  },
+  BUSD: {
+    address: "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
+    decimal: 18,
+    name: "Binance USD",
+    logo: "https://tokens.1inch.exchange/0x4fabb145d64652a948d72533023f6e7a623c7c53.png",
+  },
+  DAI: {
+    address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    name: "DAI StableCoin",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0x6b175474e89094c44da98b954eedeac495271d0f.png",
+  },
+  ENJ: {
+    address: "0xF629cBd94d3791C9250152BD8dfBDF380E2a3B9c",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0xf629cbd94d3791c9250152bd8dfbdf380e2a3b9c.png",
+    name: "Enjin Coin",
+  },
+  KNC: {
+    address: "0xdd974D5C2e2928deA5F71b9825b8b646686BD200",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0xdd974d5c2e2928dea5f71b9825b8b646686bd200.png",
+    name: "Kyber Network",
+  },
+  LINK: {
+    address: "0x514910771AF9Ca656af840dff83E8264EcF986CA",
+    decimal: 18,
+    name: "Chain Link",
+    logo: "https://tokens.1inch.exchange/0x514910771af9ca656af840dff83e8264ecf986ca.png",
+  },
+  MANA: {
+    address: "0x0F5D2fB29fb7d3CFeE444a200298f468908cC942",
+    decimal: 18,
+    name: "Mana",
+    logo: "https://tokens.1inch.exchange/0x0f5d2fb29fb7d3cfee444a200298f468908cc942.png",
+  },
+  MKR: {
+    address: "0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2.png",
+    name: "Maker"
+  },
+  REN: {
+    address: "0x408e41876cCCDC0F92210600ef50372656052a38",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0x408e41876cccdc0f92210600ef50372656052a38.png",
+    name: "Republic",
+  },
+  SNX: {
+    address: "0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f.png",
+    name: "Synthetix Network Token",
+  },
+  sUSD: {
+    address: "0x57Ab1ec28D129707052df4dF418D58a2D46d5f51",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0x57ab1ec28d129707052df4df418d58a2d46d5f51.png",
+    name: "Synth sUSD",
+  },
+  TUSD: {
+    address: "0x0000000000085d4780B73119b644AE5ecd22b376",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0x0000000000085d4780b73119b644ae5ecd22b376.png",
+    name: "TrueUSD"
+  },
+  USDC: {
+    address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    decimal: 6,
+    logo: "https://tokens.1inch.exchange/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
+    name: "USD Coin"
+  },
+  CRV: {
+    address: "0xD533a949740bb3306d119CC777fa900bA034cd52",
+    decimal: 18,
+    logo: "https://tokens.1inch.exchange/0xd533a949740bb3306d119cc777fa900ba034cd52.png",
+    name: "Curve DAO Token",
+  },
+  GUSD: {
+    address: "0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd",
+    decimal: 2,
+    logo: "https://tokens.1inch.exchange/0x056fd409e1d7a124bd7017459dfea2f387b6d5cd.png",
+    name: "Gemini Dollar",
+  },
+};
 // for 1inch tokens
 
 // const getApprove1inchData = async (tokenAddress) => {
--- packages/react-app/src/views/NewUI.jsx
+++ packages/react-app/src/views/NewUI.jsx
@@ -12,6 +12,7 @@ import { abi as AAVEIDataProvider } from "../abis/AAVEProtocolDataProvider.json"
 import {
   getListOfTokensSupportedByAAVE,
   // getApprove1inchData,
+  tokenData,
   get1InchQuote,
   get1InchSwapData,
 } from "../helpers/abiHelpers";
@@ -314,7 +315,25 @@ function NewUI(props) {
     <Layout>
       {/* <Positions signer={signer} /> */}
       <Header className="header-levaave">
-        <div className="header-main-text">LevAave</div>
+        <div style={{ marginLeft: '40px'}}>
+          <svg width="28px" height="36px" viewBox="0 0 28 36" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <title>noun_Ghost_2076593 Copy</title>
+              <g id="Proto-V5-Green" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g id="2" transform="translate(-31.000000, -17.000000)" fill="#FFFFFF" fill-rule="nonzero">
+                      <g id="noun_Ghost_2076593-Copy" transform="translate(31.000000, 17.000000)">
+                          <path d="M23.688085,4.19889132 C21.1529053,1.70332384 17.7066455,0.158448729 13.8642638,0.158448729 C6.21911261,0.158448729 0,6.37756134 0,14.0227125 L0,27.8666662 C0,28.4212367 0.237673093,28.8965829 0.594182733,29.2530926 C0.950692373,29.6096022 1.46565074,29.8472753 1.98060911,29.8472753 C3.08975021,29.8472753 3.96121822,28.9758073 3.96121822,27.8666662 L3.96121822,25.1141235 C3.96121822,24.559553 4.19889132,24.0842068 4.55540096,23.7276972 C4.9119106,23.3711875 5.42686896,23.1335144 5.94182733,23.1335144 C7.05096844,23.1335144 7.92243644,24.0049824 7.92243644,25.1141235 L7.92243644,31.4520727 L7.92243644,33.5515183 C7.92243644,34.6606594 8.79390445,35.5321275 9.90304556,35.5321275 C11.0121867,35.5321275 11.8836547,34.6606594 11.8836547,33.5515183 L11.8836547,33.4326818 L11.8836547,25.1141235 C11.8836547,24.0049824 12.7551227,23.1335144 13.8642638,23.1335144 C14.4188343,23.1335144 14.8941805,23.3711875 15.2506902,23.7276972 C15.6071998,24.0842068 15.8448729,24.5991652 15.8448729,25.1141235 L15.8448729,26.2106522 C15.8448729,27.3197933 16.7163409,28.1912613 17.825482,28.1912613 C18.9346231,28.1912613 19.8060911,27.3197933 19.8060911,26.2106522 L19.8060911,25.1141235 C19.8060911,24.0049824 20.6775591,23.1335144 21.7867002,23.1335144 C22.3412708,23.1335144 22.816617,23.3711875 23.1731266,23.7276972 C23.5296362,24.0842068 23.7673093,24.5991652 23.7673093,25.1141235 L23.7673093,30.4993719 C23.7673093,31.608513 24.6387773,32.479981 25.7479184,32.479981 C26.8570595,32.479981 27.7285276,31.608513 27.7285276,30.4993719 L27.7285276,23.1335144 L27.7285276,14.0227125 C27.7285276,10.1803308 26.1836524,6.73407098 23.688085,4.19889132 Z M9.0913798,11.933083 C7.22249289,11.933083 5.6934036,10.4039937 5.6934036,8.53510683 C5.6934036,6.66621993 7.22249289,5.13713064 9.0913798,5.13713064 C10.9602667,5.13713064 12.489356,6.66621993 12.489356,8.53510683 C12.489356,10.4464684 10.9602667,11.933083 9.0913798,11.933083 Z M13.910285,20.0241993 C10.6058202,20.0241993 7.86783521,17.3892063 7.86783521,14.1070221 L9.80330739,14.1070221 L10.7474402,16.041112 C10.9834734,16.4571635 11.5971597,16.4571635 11.8331929,16.041112 L12.7773256,14.1070221 L14.948831,14.1070221 L15.8929638,16.041112 C16.128997,16.4571635 16.7426833,16.4571635 16.9787165,16.041112 L17.9228492,14.1070221 L19.8583214,14.1070221 C19.9527347,17.3892063 17.2147497,20.0241993 13.910285,20.0241993 Z M18.5206652,11.933083 C16.8716473,11.933083 15.5224509,10.5838866 15.5224509,8.93486874 C15.5224509,7.28585088 16.8716473,5.93665445 18.5206652,5.93665445 C20.169683,5.93665445 21.5188795,7.28585088 21.5188795,8.93486874 C21.5188795,10.6213643 20.169683,11.933083 18.5206652,11.933083 Z" id="Shape"></path>
+                      </g>
+                  </g>
+              </g>
+          </svg>
+        </div>
+        <div className="header-main-text-lev">Lev</div>
+        <div className="header-main-text-aave">Aave</div>
+        <div className="connect-button-div">
+          <button className="connect-button">
+            Connect-Wallet
+          </button>
+        </div>
       </Header>
       <Content className="main-background">
         <>
@@ -377,7 +396,7 @@ function NewUI(props) {
                       />
                       <button id="collateral-token-dd" className="swap-page-input-body-button">
                         <span className="swap-page-input-body-button-main-span">
-                          {/* <img className="swap-page-input-body-button-img"></img> */}
+                          <img className="swap-page-input-body-button-img" src={tokenData[selectedCollateralCurrencyType.label].logo}></img>
                           <span className="swap-page-input-body-button-text">
                             {selectedCollateralCurrencyType.label}{" "}
                           </span>
@@ -400,40 +419,44 @@ function NewUI(props) {
                 <div className="postion-leverage-div">
                   {/* Position */}
                   <div className="position-div">
-                    <div className="position-label-div">Position:</div>
-                    <div
-                      onClick={() => onChangeLeverageType("long")}
-                      className={clsx({ "position-box": true, active: leverageType === "long" })}
-                    >
-                      Long
-                    </div>
-                    <div
-                      onClick={() => onChangeLeverageType("short")}
-                      className={clsx({ "position-box": true, active: leverageType === "short" })}
-                    >
-                      Short
+                    <div className="position-label-div">Position</div>
+                    <div style={{ display: 'flex'}}>
+                      <div
+                        onClick={() => onChangeLeverageType("long")}
+                        className={clsx({ "position-box": true, active: leverageType === "long" })}
+                      >
+                        Long
+                      </div>
+                      <div
+                        onClick={() => onChangeLeverageType("short")}
+                        className={clsx({ "position-box": true, active: leverageType === "short" })}
+                      >
+                        Short
+                      </div>
                     </div>
                   </div>
 
                   <div className="leverage-div">
                     <div className="leverage-label-div">Leverage:</div>
-                    <div
-                      className={clsx({ "leverage-box": true, active: leverageMultiplier === 1 })}
-                      onClick={() => updateLeverageMultiplier(1)}
-                    >
-                      2x
-                    </div>
-                    <div
-                      className={clsx({ "leverage-box": true, active: leverageMultiplier === 2 })}
-                      onClick={() => updateLeverageMultiplier(2)}
-                    >
-                      3x
-                    </div>
-                    <div
-                      className={clsx({ "leverage-box": true, active: leverageMultiplier === 3 })}
-                      onClick={() => updateLeverageMultiplier(3)}
-                    >
-                      4x
+                    <div style={{ display: 'flex'}}>
+                      <div
+                        className={clsx({ "leverage-box": true, active: leverageMultiplier === 1 })}
+                        onClick={() => updateLeverageMultiplier(1)}
+                      >
+                        2x
+                      </div>
+                      <div
+                        className={clsx({ "leverage-box": true, active: leverageMultiplier === 2 })}
+                        onClick={() => updateLeverageMultiplier(2)}
+                      >
+                        3x
+                      </div>
+                      <div
+                        className={clsx({ "leverage-box": true, active: leverageMultiplier === 3 })}
+                        onClick={() => updateLeverageMultiplier(3)}
+                      >
+                        4x
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -444,7 +467,7 @@ function NewUI(props) {
                     {/* Input Leverage Label */}
                     <div className="swap-page-input-label-div">
                       <div className="swap-page-input-label-text-div">
-                        <div className="swap-page-input-label-text">Leverage Asset</div>
+                        <div className="swap-page-input-label-text">Tokens Received</div>
                       </div>
                     </div>
 
@@ -469,7 +492,7 @@ function NewUI(props) {
                       ></input>
                       <button id="leverage-token-dd" className="swap-page-input-body-button">
                         <span className="swap-page-input-body-button-main-span">
-                          {/* <img className="swap-page-input-body-button-img"></img> */}
+                          <img className="swap-page-input-body-button-img" src={tokenData[selectedLeverageCurrencyType.label].logo}></img>
                           <span className="swap-page-input-body-button-text">{selectedLeverageCurrencyType.label}</span>
                           <svg
                             width="12"
@@ -513,8 +536,11 @@ function NewUI(props) {
             target="collateral-token-dd"
             toggle={() => updateIsSelectingCollateralCurrency(!isSelectingCollateralCurrency)}
             hideArrow
+            popperClassName="custom-popover"
+            innerClassName="custom-inner-popover"
+            fade={true}
           >
-            <PopoverBody>
+            <PopoverBody className="popover-body">
               <div className="dd-wrapper">
                 {tokenOptionsForLeveraging.map(option => {
                   return (
@@ -547,8 +573,9 @@ function NewUI(props) {
             target="leverage-token-dd"
             toggle={() => updateIsSelectingLeverageCurrency(!isSelectingLeverageCurrency)}
             hideArrow
+            className="popover-main-body"
           >
-            <PopoverBody>
+            <PopoverBody style={{padding: '0px'}}>
               <div className="dd-wrapper">
                 {tokenOptionsForLeveraging.map(option => {
                   return (
