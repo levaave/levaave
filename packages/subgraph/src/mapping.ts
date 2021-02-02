27f01a9d19aaded4ca621ff596ed9229bf3cea7b
Bhanu Sanghi <bhanu@jiny.io>
2021-02-02 23:22:08 +0530

    Added subgraph

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,34 +1,43 @@
-import { BigInt, Address } from "@graphprotocol/graph-ts"
-import {
-  YourContract,
-  TransactionSuccess
-} from "../generated/YourContract/YourContract"
-import { Purpose, Sender } from "../generated/schema"
+import { BigInt, Address } from "@graphprotocol/graph-ts";
+import { TransactionSuccess } from "../generated/LevAave/LevAave";
+import { Transaction, User } from "../generated/schema";
 
 export function handleTransactionSuccess(event: TransactionSuccess): void {
-
-  let senderString = event.params.sender.toHexString()
-
-  let sender = Sender.load(senderString)
-
-  if (sender == null) {
-    sender = new Sender(senderString)
-    sender.address = event.params.sender
-    sender.createdAt = event.block.timestamp
-    sender.purposeCount = BigInt.fromI32(1)
+  let userId = event.params.userAddress.toHexString();
+  let transactions: Transaction[];
+  // let leverageType = event.params.userAddress.toHexString();
+  let user = User.load(userId);
+  if (user == null) {
+    user = new User(userId);
+    user.address = event.params.userAddress.toString();
+    // user.createdAt = event.block.timestamp;
+  } else {
+    // transactions = <Array<Transaction>>(<unknown>user.transactions);
   }
-  else {
-    sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1))
-  }
-
-  let purpose = new Purpose(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
-
-  purpose.purpose = event.params.purpose
-  purpose.sender = senderString
-  purpose.createdAt = event.block.timestamp
-  purpose.transactionHash = event.transaction.hash.toHex()
-
-  purpose.save()
-  sender.save()
-
+  addNewTransaction(userId, event);
+  // transactions.push(newTransaction);
+  // user.transactions = transactions;
+  // user.save();
 }
+
+function addNewTransaction(
+  userId: string,
+  event: TransactionSuccess
+): Transaction {
+  let transaction = new Transaction("1");
+  transaction.user = userId;
+  transaction.leverageType = event.params.leverageType;
+  transaction.leverageMultiplier = event.params.leverageMultiplier;
+  transaction.isActive = true;
+  transaction.debtTokenAddress = event.params.collateralTokenAddress.toHexString();
+  transaction.debtValue = event.params.collateralAmount;
+  transaction.leverageTokenAddress = event.params.leverageTokenAddress.toHexString();
+  transaction.leverageValue = event.params.leverageAmount;
+  // transaction.createdAt = event.block.timestamp;
+  // transaction.modifiedAt = event.block.timestamp;
+  transaction.save();
+  return transaction;
+}
+// function getRandomInt(max: number): string {
+//   return String(Math.floor(Number(Math.random() * max)));
+// }
